b0bf24990971dd14ceb948c1f61c365b8f0a2b2c
Weverton Paulino <wevertoncesar@gmail.com>
2020-12-21 18:46:11 -0300

    Adiciona entidade

---
+++ DocMvc.Domain/Entities/Documento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DocMvc.Domain.Entities
+{
+    public class Documento
+    {
+        public int Codigo { get; set; }
+        public string Titulo { get; set; }
+        public string Revisao { get; set; }
+        public DateTime DataPlanejada { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
