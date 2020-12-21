761c7880fe7f8ec65d78ff11288ee382177dea9d
Weverton Paulino <wevertoncesar@gmail.com>
2020-12-21 19:33:18 -0300

    Implementa configurações

---
+++ DocMvc.Infrastructure.Data/Configuration/DocumentoConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using DocMvc.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace DocMvc.Infrastructure.Data.Configuration
+{
+    public class DocumentoConfiguration : EntityTypeConfiguration<Documento>
+    {
+        public DocumentoConfiguration()
+        {
+            HasKey(p => p.Codigo);
+
+            Property(p => p.Titulo)
+                .IsRequired();
+
+            Property(p => p.Revisao)
+                .IsRequired()
+                .HasMaxLength(1);
+
+            Property(p => p.DataPlanejada)
+                .IsRequired();
+
+            Property(p => p.Valor)
+                .IsRequired()
+                .HasPrecision(18, 2);
+        }
+    }
+}
