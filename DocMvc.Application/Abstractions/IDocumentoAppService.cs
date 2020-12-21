8baa36b03b423bfde252a9c14e377740f66f523c
Weverton Paulino <wevertoncesar@gmail.com>
2020-12-21 19:56:09 -0300

    Adiciona camada de abstração

---
+++ DocMvc.Application/Abstractions/IDocumentoAppService.cs
@@ -0,0 +1,10 @@
+﻿using DocMvc.Domain.Entities;
+using System.Collections.Generic;
+
+namespace DocMvc.Application.Abstractions
+{
+    public interface IDocumentoAppService : IAppServiceBase<Documento>
+    {
+        IEnumerable<Documento> GetByTitulo(string titulo);
+    }
+}
