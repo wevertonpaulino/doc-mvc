e8fcee89a63a4a1be8fbad79de8490e273b68d37
Weverton Paulino <wevertoncesar@gmail.com>
2020-12-21 19:08:33 -0300

    Implementa classes concretas

---
+++ DocMvc.Domain/Services/DocumentoService.cs
@@ -0,0 +1,23 @@
+﻿using DocMvc.Domain.Abstractions.Repositories;
+using DocMvc.Domain.Abstractions.Services;
+using DocMvc.Domain.Entities;
+using System.Collections.Generic;
+
+namespace DocMvc.Domain.Services
+{
+    public class DocumentoService : ServiceBase<Documento>, IDocumentoService
+    {
+        private readonly IDocumentoRepository _documentoRepository;
+
+        public DocumentoService(IDocumentoRepository documentoRepository)
+            : base(documentoRepository)
+        {
+            _documentoRepository = documentoRepository;
+        }
+
+        public IEnumerable<Documento> GetByTitulo(string titulo)
+        {
+            return _documentoRepository.GetByTitulo(titulo);
+        }
+    }
+}
