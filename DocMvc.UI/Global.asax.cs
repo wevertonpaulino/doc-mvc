5b7db624ed70511ca5627c700837b2f77b6b0cfd
Weverton Paulino <wevertoncesar@gmail.com>
2020-12-21 20:58:44 -0300

    Registra AutoMapper

--- DocMvc.UI/Global.asax.cs
+++ DocMvc.UI/Global.asax.cs
@@ -1,6 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using DocMvc.UI.Mapping;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -8,7 +6,7 @@ using System.Web.Routing;
 
 namespace DocMvc.UI
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
@@ -16,6 +14,7 @@ namespace DocMvc.UI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AutoMapperConfig.RegisterMappings();
         }
     }
 }
