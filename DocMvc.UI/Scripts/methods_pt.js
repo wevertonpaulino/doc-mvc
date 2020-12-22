ba0401adc4d749296cfc63bca306d5b21a3ff536
Weverton Paulino <wevertoncesar@gmail.com>
2020-12-21 21:43:55 -0300

    Implementa validações

---
+++ DocMvc.UI/Scripts/methods_pt.js
@@ -0,0 +1,19 @@
+﻿/*
+ * Localized default methods for the jQuery validation plugin.
+ * Locale: PT_BR
+ */
+jQuery.extend(jQuery.validator.methods, {
+    date: function (value, element) {
+        if ($.browser.webkit) {
+            var d = new Date();
+
+            return this.optional(element) || !/Invalid|NaN/.test(new Date(d.toLocaleDateString(value)));
+        }
+        else {
+            return thiw.optional(element) || !/Invalid|NaN/.test(new Date(value));
+        }
+    },
+    number: function (value, element) {
+        return this.optional(element) || /^-?(?:\d+|\d{1,3}(?:\.\d{3})+)(?:,\d+)?$/.test(value);
+    }
+});
